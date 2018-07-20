ddd1a22d89b5d273ab87e312a194e4260c884634
Kaan Donbekci <kaandonbekci@Kaans-MacBook-Pro.local>
2018-07-20 10:49:04 -0700

    threads can support infinite jobs

--- .common-lib/multithread.py
+++ .common-lib/multithread.py
@@ -1,6 +1,7 @@
 import threading
 import time
 import random
+from collections import deque
 # TYPES = {
 # 'AUDIOTORY': 1,
 # 'OLFACTORY': 2,
@@ -10,26 +11,43 @@ import random
 # }
 
 class Intervention(threading.Thread):
-    def __init__(self, type, no, func):
+    def __init__(self, type, no, funcs, delay = None, delayFunc = None):
         threading.Thread.__init__(self)
         self.name = type+'_'+str(no)
-        self.func = func
+        self.funcs = deque()
+        self.delay = delay
+        [self.funcs.append(i) for i in funcs]
+        # print(self.funcs)
+
     def run(self):
-        self.func(self)
+        next = self.funcs.popleft()
+        while next is not None:
+            next(self)
+            self.funcs.append(next)
+            next = self.funcs.popleft()
+            if self.delay is not None:
+                delayFunc(delay)
 
-# def getTime(thread):
-#     print("Thread {} sleeps at {}".format(thread.name,
-#         time.strftime("%H:%M:%S", time.gmtime())))
-#     randSleepTime = random.randint(1, 5)
-#     time.sleep(randSleepTime)
-#     print("Thread {} wakes at {}".format(thread.name,
-#         time.strftime("%H:%M:%S", time.gmtime())))
-
-# def test():
-#     thread1 = Intervention('AUDIOTORY', 1, getTime)
-#     thread2 = Intervention('VISUAL', 1, getTime)
-#     thread3 = Intervention('OLFACTORY', 1, getTime)
-#     thread1.start()
-#     thread2.start()
-#     thread3.start()
-# test()
+def getTime(thread):
+    print("{} sleeps at {}".format(thread.name,
+        time.strftime("%H:%M:%S", time.gmtime())))
+    randSleepTime = random.randint(1, 5)
+    time.sleep(2)
+    print("{} wakes at {}".format(thread.name,
+        time.strftime("%H:%M:%S", time.gmtime())))
+def makeCoffee(thread):
+    print("{} is making coffe at {}".format(thread.name,
+        time.strftime("%H:%M:%S", time.gmtime())))
+    randSleepTime = random.randint(1, 5)
+    time.sleep(2)
+    print("{} sips the coffe, yummy at {}".format(thread.name,
+        time.strftime("%H:%M:%S", time.gmtime())))
+def test():
+    funcs = [getTime, makeCoffee]
+    thread1 = Intervention('AUDIOTORY', 1, funcs)
+    thread2 = Intervention('VISUAL', 1, funcs)
+    thread3 = Intervention('OLFACTORY', 1, funcs)
+    thread1.start()
+    thread2.start()
+    thread3.start()
+test()
