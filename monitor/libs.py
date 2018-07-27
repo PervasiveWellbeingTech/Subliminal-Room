8b2876040ee050b54f28160c5b9f6ec45d635836
Kaan Donbekci <kaandonbekci@Kaans-MacBook-Pro.local>
2018-07-27 16:22:17 -0700

    psychopy

--- monitor/libs.py
+++ monitor/libs.py
@@ -1,7 +1,13 @@
 import sys
+import signal
 sys.path.insert(0, '/Users/kaandonbekci/dev/pervasivetech/Room/visual/lib')
 sys.path.insert(0, '/Users/kaandonbekci/dev/pervasivetech/Room/audiotory/lib')
 sys.path.insert(0, '/Users/kaandonbekci/dev/pervasivetech/Room/tactile/lib')
 sys.path.insert(0, '/Users/kaandonbekci/dev/pervasivetech/Room/olfactory/lib')
 sys.path.insert(0, '/Users/kaandonbekci/dev/pervasivetech/Room/monitor/lib')
 sys.path.insert(0, '/Users/kaandonbekci/dev/pervasivetech/Room/.common-lib')
+def signal_handler(sig, frame):
+    print
+    print('Exiting')
+    sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
