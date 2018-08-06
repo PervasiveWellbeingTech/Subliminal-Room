1e49fee3ab85f43fb1761351bb1ac76e18c0a42f
Kaan Donbekci <kaandonbekci@Kaans-MBP.lan>
2018-08-06 13:03:12 -0700

    pilto

--- monitor/lib/zephyr/delayed_stream.py
+++ monitor/lib/zephyr/delayed_stream.py
@@ -13,32 +13,32 @@ class DelayedRealTimeStream(threading.Thread):
         self.callbacks = callbacks
         self.default_delay = default_delay
         self.specific_delays = specific_delays
-        
+
         self.stream_output_positions = collections.defaultdict(lambda: 0)
-        
+
         self.terminate_requested = False
-    
+
     def add_callback(self, callback):
         self.callbacks.append(callback)
-    
+
     def terminate(self):
         self.terminate_requested = True
-    
+
     def run(self):
         while not self.terminate_requested:
             now = zephyr.time()
             all_streams = itertools.chain(self.signal_collector.iterate_signal_stream_histories(),
                                           self.signal_collector.iterate_event_streams())
-            
+
             for signal_stream_name, signal_stream_history in all_streams:
                 delay = self.specific_delays.get(signal_stream_name, self.default_delay)
-                
+
                 delayed_current_time = now - delay
-                
+
                 from_sample = self.stream_output_positions[signal_stream_name]
                 for sample in signal_stream_history.iterate_samples(from_sample, delayed_current_time):
                     self.stream_output_positions[signal_stream_name] += 1
                     for callback in self.callbacks:
                         callback(signal_stream_name, sample)
-            
+
             time.sleep(0.01)
--- monitor/read_from_device.py
+++ monitor/read_from_device.py
@@ -1,25 +1,25 @@
 import libs
 import serial
 import platform
-
 import zephyr
+import time
 from zephyr.testing import simulation_workflow
 
 
 def callback(value_name, value):
-    print(value_name, value)
+    # if value_name =
+    print('({}: {})'.format(value_name, value))
 
 def main():
-    zephyr.configure_root_logger()
-
     serial_port_dict = {"Darwin": "/dev/tty.BHBHT022509-iSerialPort1",
                         "Windows": 23}
 
     serial_port = serial_port_dict[platform.system()]
-    ser = serial.Serial(serial_port)
-
+    ser = serial.Serial(serial_port) #establish serial connection
+    # time.sleep(10)
     simulation_workflow([callback], ser)
-    
+
+
 
 if __name__ == "__main__":
     main()
